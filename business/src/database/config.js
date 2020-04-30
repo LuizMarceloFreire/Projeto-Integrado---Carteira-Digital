5deae2f0fc27c4908a3cbba73d1a29d3469544ea
Thiago Augusto <thiagoggth@gmail.com>
2020-04-30 16:07:45 -0400

    Restrutrurada diretorio database

---
+++ business/src/database/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    database: 'CarteiraDigital',
+    user: 'root',
+    password: '',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorAliases: false,
+    define: {
+        timestamps: true,
+    }
+};
