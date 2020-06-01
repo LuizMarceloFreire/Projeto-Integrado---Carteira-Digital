6bd896b4cdad8463d77d8e72307826c5352f0e78
Thiago Augusto <thiagoggth@gmail.com>
2020-06-01 14:17:28 -0400

    implementado conexão a api

--- mobile/src/views/Login/index.js
+++ mobile/src/views/Login/index.js
@@ -2,16 +2,39 @@ import React, { useState } from 'react';
 import { Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
 import styles from './styles';
 
+import api from '../../services/api';
+
 import logo from '../../assets/img/logo.png';
 import NeedHelp from '../../components/NeedHelp';
+
 
 const Login = ({ navigation }) => {
 
     const [cpf, setCpf] = useState('');
+    let senha = '';
 
     const setClearNumber = (number) => {
         setCpf(number.replace(/[^0-9]/g, ""));
     };
+    const isCpf = () => {
+        return true
+    };
+
+    const login = () => {
+        console.log(senha);
+        if (cpf && senha && isCpf()) {
+            api.post('/login', {
+                cpf,
+                senha
+            }).then(result => {
+                if (result.data.result) {
+                    console.log('senha correta');
+                } else {
+                    console.log('!');
+                }
+            });
+        }
+    }
 
     return (
         <View style={styles.loginWrapper}>
@@ -31,11 +54,12 @@ const Login = ({ navigation }) => {
                         onChange={(e) => { setClearNumber(e.nativeEvent.text) }}
                     />
                     <TextInput
+                        secureTextEntry={true}
                         style={[styles.inputText, styles.inputTextLest]}
                         underlineColorAndroid='rgba(0,0,0,0)'
                         placeholder='Senha'
                         placeholderTextColor='#CCC'
-
+                        onChange={(e) => { senha = e.nativeEvent.text }}
                     />
                     <View style={styles.buttonsWrapper}>
                         <TouchableOpacity
@@ -44,7 +68,10 @@ const Login = ({ navigation }) => {
                         >
                             <Text style={styles.buttonJoinText}>Cadastre-se</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.buttonLogin}>
+                        <TouchableOpacity
+                            style={styles.buttonLogin}
+                            onPress={login}
+                        >
                             <Text style={styles.buttonLoginText}>Login</Text>
 
                         </TouchableOpacity>
