5559d3a313fcdbd6a301a5afab66bb6b81ad37af
Thiago Augusto <thiagoggth@gmail.com>
2020-06-01 12:46:23 -0400

    implementado validação de login e cadastro de usuario

---
+++ business/src/controllers/UsuariosController.js
@@ -0,0 +1,56 @@
+const Usuarios = require('../models/Usuarios');
+
+module.exports = {
+    async login(req, res) {
+        const { cpf, senha } = req.body;
+
+        if (!cpf || !senha) {
+            return res.status(200).json({
+                message: 'envie todos os valores obrigatorios!'
+            });
+        }
+
+        const usuario = await Usuarios.findOne({
+            where: {
+                cpf
+            }
+        });
+
+        const userPassword = usuario.senha
+
+        if (userPassword === senha) {
+            return res.status(200).json({ result: true });
+        } else {
+            return res.status(200).json({ result: false });
+        }
+    },
+
+    async create(req, res) {
+        const { nome, cpf, email, senha } = req.body;
+
+        if (!nome || !cpf || !email || !senha) {
+            return res.status(200).json({
+                message: 'envie todos os valores obrigatorios!'
+            });
+        }
+
+        const usuario = await Usuarios.findOne({
+            where: {
+                cpf
+            }
+        });
+
+        if (usuario) {
+            return res.json({ message: 'este CPF ja esta cadastrado!' });
+        }
+
+        await Usuarios.create({
+            nome,
+            cpf,
+            email,
+            senha,
+        });
+
+        return res.status(200).end();
+    },
+}
--- business/src/models/Usuarios.js
+++ business/src/models/Usuarios.js
@@ -5,11 +5,9 @@ class Usuarios extends Model {
     static init(sequelize) {
         super.init({
             nome: DataTypes.STRING(),
-            data: DataTypes.DATE(),
             cpf: DataTypes.STRING(11),
             email: DataTypes.STRING(),
-            telefone: DataTypes.STRING(),
-            sexo: DataTypes.STRING(1),
+            senha: DataTypes.STRING(),
         }, {
             sequelize
         });
