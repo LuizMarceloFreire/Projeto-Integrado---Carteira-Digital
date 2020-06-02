ab5fe34950acf626a60a3f76afaef966cc7c8698
Thiago Augusto <thiagoggth@gmail.com>
2020-06-02 19:25:21 -0400

    implementado requisição para criar novos documentos

--- business/src/controllers/DecumentosController.js
+++ business/src/controllers/DecumentosController.js
@@ -1,14 +1,43 @@
 const Documentos = require('../models/Documentos');
 
 module.exports = {
-    async index(req, res) {
+    async pegarDocumento(req, res) {
         const documentos = await Documentos.findAll();
 
         return res.json(documentos);
     },
 
-    async store(req, res) {
-        const documentos = await Documentos.create({ imagem, usuarioId, tipoDocumentoId });
+    async create(req, res) {
+
+        const { tipoDocumentoId, usuarioId } = req.body;
+        const imagem = req.file.buffer;
+
+        const jaExiste = await Documentos.findOne({
+            where: {
+                usuarioId,
+                tipoDocumentoId,
+            }
+        });
+
+        if (jaExiste) {
+            await Documentos.update(
+                {
+                    imagem,
+                },
+                {
+                    where: {
+                        id: jaExiste.id
+                    }
+                }
+            );
+        } else {
+            await Documentos.create({
+                tipoDocumentoId,
+                usuarioId,
+                imagem,
+            });
+        };
+
 
         return res.send('Concluido!');
     },
--- business/src/routes.js
+++ business/src/routes.js
@@ -1,7 +1,21 @@
 const express = require('express');
-const UsuariosController = require('./controllers/UsuariosController');
+const muter = require('multer');
+const storage = muter.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/');
+    },
 
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+const memoruStorage = muter.memoryStorage();
+
+const upload = muter({ memoruStorage });
 const routes = express.Router();
+
+const UsuariosController = require('./controllers/UsuariosController');
+const DocumentosController = require('./controllers/DecumentosController');
 
 routes.get('/', (request, response) => {
     response.send({
@@ -10,6 +24,9 @@ routes.get('/', (request, response) => {
 });
 
 routes.post('/login', UsuariosController.login);
+
+routes.get('/documentos', DocumentosController.pegarDocumento);
+routes.post('/documentos', upload.single('imagem'), DocumentosController.create);
 
 routes.post('/usuarios', UsuariosController.create);
 
